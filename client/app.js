dcd801375ae2a277ad09273c829a28e6b22ea32c
Jinwei Lin <jinwei.lin8@gmail.com>
2017-01-19 12:32:18 -0500

    add authitacation

---
+++ client/app.js
@@ -0,0 +1,43 @@
+angular.module('doggybook', [
+  'doggybook.services',
+  'ngRoute'
+])
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/signin', {
+      templateUrl: 'app/auth/signin.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+
+    .otherwise({
+      redirectTo: '/dashboard'
+    });
+
+  $httpProvider.interceptors.push('AttachTokens');
+})
+.factory('AttachTokens', function ($window) {
+
+  var attach = {
+    request: function (object) {
+      var jwt = $window.localStorage.getItem('com.doggybook');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function ($rootScope, $location, Auth) {
+
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
