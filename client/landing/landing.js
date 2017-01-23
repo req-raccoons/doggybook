69e4d519473084e559538e864de9f08dca88eb15
Kyle Cameron <1kylecameron@gmail.com>
2017-01-23 13:34:44 -0500

    Debugging various front-end issues including NgRoute installation, naming conventions, and controllers/factories.

--- client/landing/landing.js
+++ client/landing/landing.js
@@ -2,9 +2,9 @@
 // we might want to run a check here that checks if a user is signed in;
   // if so, display a signout link instead of a signin link, also display links to profile & search
 
-angular.module('doggybook.landing', [])
+angular.module('doggyBook.landing', [])
 
-.controller('LandingController', function($scope,Players) {
+.controller('LandingController', function($scope, Players) {
   //only starting the file -> should be able to route to other files; nothing else for now;
   $scope.data = {};
   $scope.landingFunction = function () {
