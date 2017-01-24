ca3c5be9814c700360662e1faa1527c6cd9f34a7
Kevin Fung <kevinmcfung@gmail.com>
2017-01-24 15:22:46 -0500

    user verification on signin working

--- app/models/user.js
+++ app/models/user.js
@@ -9,44 +9,17 @@ var User = db.Model.extend({
     this.on('creating', this.hashPassword);
   },
   comparePassword: function(attemptedPassword, callback) {
-    var hashcheck = Promise.promisify(bcrypt.compare);
-
-    hashcheck(attemptedPassword, this.get('password'))
-    .then(function(isMatch) {
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
       callback(isMatch);
     });
-    // bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-    // });
   },
   hashPassword: function() {
-    console.log(`'this' is: `, this);
     var cipher = Promise.promisify(bcrypt.hash);
-
     return cipher(this.get('password'), null, null).bind(this)
       .then(function(hash) {
         this.set('password', hash);
       });
-  },
+  }
 });
 
 module.exports = User;
-
-
-
-// var hash = bcrypt.hashSync("bacon");
-//
-// bcrypt.compareSync("bacon", hash); // true
-// bcrypt.compareSync("veggies", hash); // false
-// Asynchronous
-//
-// bcrypt.hash("bacon", null, null, function(err, hash) {
-//     // Store hash in your password DB.
-// });
-//
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
