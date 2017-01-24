b66970b65cc39a1783da4aac8ada500493c9d620
Kyle Cameron <1kylecameron@gmail.com>
2017-01-24 17:25:12 -0500

    committing slight additions before rebasing. Working through prof.js

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -3,14 +3,14 @@ angular.module('doggyBook.profile', [])
 .controller('ProfController', function ($scope, Prof) {
   // userID identifies the given user
   $scope.userID = {};
-  // data object stores all of the info for the user from the db
-  $scope.data = {};
+  // profile object stores all of the info for the user from the db
+  $scope.profile = {};
 
   var profileRender = function () {
     Prof.showProf($scope.userID)
     // showProfile is a function in the Profiles factory that queries the database for a given userID
       .then(function (profData) {
-        $scope.data = profData;
+        $scope.profile = profData;
       })
       .catch(function (error) {
         console.error(error);
