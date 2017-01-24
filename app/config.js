50aae9238349927a062bbce6d4569eda671760f4
Kevin Fung <kevinmcfung@gmail.com>
2017-01-24 16:14:05 -0500

    display profile api now working

--- app/config.js
+++ app/config.js
@@ -17,7 +17,7 @@ db.knex.schema.hasTable('users')
       user.boolean('isDog');
       user.timestamps();
     }).then(function(table) {
-      console.log('Created Table: ', table);
+      console.log(`Created 'users' Table`);
     });
   }
 });
@@ -29,10 +29,11 @@ db.knex.schema.hasTable('dogs')
       // structure of dog db object to be fleshed out further
       dog.increments('id').primary();
       dog.string('name');
+      dog.integer('userId').unsigned();
       dog.foreign('userId').references('users.id');
       dog.timestamps();
     }).then(function(table) {
-      console.log('Created Table: ', table);
+      console.log(`Created 'dogs' Table`);
     });
   }
 });
@@ -44,10 +45,11 @@ db.knex.schema.hasTable('walkers')
       // structure of walker db object to be fleshed out further
       walker.increments('id').primary();
       walker.string('name');
+      walker.integer('userId').unsigned();
       walker.foreign('userId').references('users.id');
       walker.timestamps();
     }).then(function(table) {
-      console.log('Created Table: ', table);
+      console.log(`Created 'walkers' Table`);
     });
   }
 });
