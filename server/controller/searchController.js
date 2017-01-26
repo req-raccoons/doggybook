91c1fa5fe9e658009b6e901abdfece62018e77ce
Kevin Fung <kevinmcfung@gmail.com>
2017-01-26 12:05:55 -0500

    userController/handler small refactor,  start search controller

---
+++ server/controller/searchController.js
@@ -0,0 +1,11 @@
+console.log('loading... searchController.js');
+
+var db = require('../../app/config');
+
+var User = require('../../app/models/user');
+var Dog = require('../../app/models/dog');
+var Walker = require('../../app/models/walker');
+
+var Users = require('../../app/collections/users');
+var Dogs = require('../../app/collections/dogs');
+var Walkers = require('../../app/collections/walkers');
