9eb66891a0df002a76717f1965a07f49c6eb1f0e
Kyle Cameron <1kylecameron@gmail.com>
2017-01-26 14:13:47 -0500

    committing changes before rebasing

--- server/controller/profController.js
+++ server/controller/profController.js
@@ -49,3 +49,24 @@ module.exports = {
     });
   }
 }
+
+// checkAuth: function (req, res, next) {
+//   var token = req.headers['x-access-token'];
+//   if (!token) {
+//     next(new Error('No token'));
+//   } else {
+//     var user = jwt.decode(token, 'secret');
+//     findUser({username: user.username})
+//       .then(function (foundUser) {
+//         if (foundUser) {
+//           res.send(200);
+//         } else {
+//           res.send(401);
+//         }
+//       })
+//       .fail(function (error) {
+//         next(error);
+//       });
+//
+//   }
+// }
