62ff36ab15574f1f8809ae1eaed741f3c828bb17
Kevin Fung <kevinmcfung@gmail.com>
2017-01-28 18:51:28 -0500

    tweak the previous commit

--- client/dashboard/search.js
+++ client/dashboard/search.js
@@ -20,8 +20,8 @@ angular.module('doggyBook.search', [])
       map: $scope.map,
     });
     $scope.markers.push(marker);
-
-.controller('SearchController', function($scope, Search) {
+  }
+// .controller('SearchController', function($scope, Search) {
   // the results object should store all of the results of the query; ie all users matching a given query
   $scope.results = {};
   // the query object will include the search parameters
